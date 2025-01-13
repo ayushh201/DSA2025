611fc090c1041806d0d78bdce3e91d5de8b96f43
Ayush Sharma <141551003+ayushh201@users.noreply.github.com>
2025-01-13 19:40:04 +0530

    Update 678. Valid Parenthesis String.cpp

--- Dynamic Programming/678. Valid Parenthesis String.cpp
+++ Dynamic Programming/678. Valid Parenthesis String.cpp
@@ -27,3 +27,33 @@ public:
         return solve(0,0,s);
     }
 };
+
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n=s.length();
+        vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
+        dp[n][0]=true;
+        for(int i=n-1;i>=0;i--){
+            for(int open=0;open<=n;open++){
+                bool isValid=false;
+                if(s[i]=='*'){
+                    isValid|=dp[i+1][open];
+                    isValid|=dp[i+1][open+1];
+                    if(open>0){
+                        isValid|=dp[i+1][open-1];
+                    }
+                }else if(s[i]=='('){
+                    isValid|=dp[i+1][open+1];
+                }else{
+                    if(open>0){
+                        isValid|=dp[i+1][open-1];
+                    }
+                }
+                dp[i][open]=isValid;
+            }
+        }
+        return dp[0][0];
+    }
+};
