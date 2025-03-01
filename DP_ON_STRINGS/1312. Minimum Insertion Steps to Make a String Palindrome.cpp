7160b59c62c121bb220631cd155e6ad4f0f76eb6
Ayush Sharma <141551003+ayushh201@users.noreply.github.com>
2025-03-01 22:22:23 +0530

    Update 1312. Minimum Insertion Steps to Make a String Palindrome.cpp

--- DP_ON_STRINGS/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
+++ DP_ON_STRINGS/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -14,3 +14,23 @@ public:
         return solve(0,n-1,s);
     }
 };
+
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n=s.length();
+        vector<vector<int>> t(n,vector<int>(n,0));
+        for(int length=2;length<=n;length++){
+            for(int i=0;i+length-1<n;i++){
+                int j=i+length-1;
+                if(s[i]==s[j]){
+                    t[i][j]=t[i+1][j-1];
+                }else{
+                    t[i][j]=1+min(t[i+1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[0][n-1];
+    }
+};
